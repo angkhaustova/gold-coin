f5190f26d246eb781e8534a0d144b5215203217f
Angelica Khaustova <angelicakhaustova@gmail.com>
2019-11-24 13:31:03 +0300

    Modals

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,4 +14,32 @@ $(document).ready(function() {
     autoplayHoverPause: true,
     center: true
   });
+
+  $("[data-open-modal]").click(e => {
+    $(".b-modal-container--open").removeClass("b-modal-container--open");
+    $(`[data-modal="${$(e.currentTarget).data("open-modal")}"]`).addClass(
+      "b-modal-container--open"
+    );
+  });
+
+  $(".b-modal__close").click(e => {
+    $(e.currentTarget)
+      .parents(".b-modal-container")
+      .removeClass("b-modal-container--open");
+  });
+
+  let startTarget;
+
+  $(".b-modal-container").on("mousedown", event => {
+    startTarget = event.target;
+  });
+
+  $(".b-modal-container").on("mouseup", event => {
+    if (
+      $(event.target).hasClass("b-modal-container") &&
+      $(startTarget).hasClass("b-modal-container")
+    ) {
+      $(".b-modal-container").removeClass("b-modal-container--open");
+    }
+  });
 });
